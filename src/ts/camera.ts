2350fda0bc2bd7b171d1450f90c2dfee99de2412
Script Raccoon <scriptraccoon@gmail.com>
2023-12-03 00:48:26 +0100

    simplify camera interface

--- src/ts/camera.ts
+++ src/ts/camera.ts
@@ -9,22 +9,22 @@ import {
 } from "./points"
 
 interface camera_interface {
-	pos: point3d
+	distance: number
 	rotation: point2d
 	zoom: number
 	project: (_: point3d) => point2d
 }
 
 export const camera: camera_interface = {
-	pos: { x: 0, y: 0, z: 10 },
+	distance: 10,
 	rotation: { x: 0, y: 0 },
 	zoom: 1000,
 	project(point: point3d): point2d {
 		const rotated_x = rotate_along_x(point, camera.rotation.x)
 		const rotated_y = rotate_along_y(rotated_x, camera.rotation.y)
 		return scale(
-			camera.zoom / (rotated_y.z + camera.pos.z),
-			add(forget_z(rotated_y), forget_z(camera.pos))
+			camera.zoom / (rotated_y.z + camera.distance),
+			forget_z(rotated_y)
 		)
 	},
 }
